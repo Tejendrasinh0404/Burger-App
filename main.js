555ccc462f363ee8ea8264b04d108b05cf0b3f44
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-04-27 08:43:24 +0530

    added menu section

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// hamburger menu for navbar 
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 const closeIcon = document.getElementById("nav-close");
@@ -15,4 +15,38 @@ navMenu.classList.remove("hidden");
 })
 closeIcon.addEventListener("click",()=>{
 navMenu.classList.add("hidden");
+})
+
+// tabs 
+const tabs = document.querySelectorAll(".tabs_wrap ul li");
+const all = document.querySelectorAll(".item__wrap");
+const foods = document.querySelectorAll(".food");
+const snacks = document.querySelectorAll(".snack");
+const beverages = document.querySelectorAll(".beverage");
+
+
+
+tabs.forEach(tab =>{
+    tab.addEventListener("click",()=>{
+        tabs.forEach(tab=>{tab.classList.remove("active")})
+        tab.classList.add("active");
+
+        const tabVal = tab.getAttribute("data-tabs");
+        all.forEach(item=>{
+            item.style.display= "none"
+        })
+        if(tabVal == "food"){
+            foods.forEach(items=>{items.style.display = "block"})
+        }
+
+       else if(tabVal == "snack"){
+            snacks.forEach(items=>{items.style.display = "block"})
+        }
+        else if(tabVal == "beverage"){
+            beverages.forEach(items=>{items.style.display = "block"})
+        }
+        else {
+            all.forEach(items=>{items.style.display = "block"})
+        }
+    })
 })
