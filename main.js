c52f55c2a57760282b7e588154e65e6835f4baa0
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-04-30 07:32:50 +0530

    added footer and more

--- main.js
+++ main.js
@@ -50,3 +50,15 @@ tabs.forEach(tab =>{
         }
     })
 })
+
+
+// show scroll up 
+const  scrollUp= ()=>{
+    const scrollUpBtn = document.getElementById("scroll-up");
+    if(scrollY >= 250){
+        scrollUpBtn.classList.remove("-bottom-1/2")
+        scrollUpBtn.classList.add("bottom-4")
+    }
+ 
+}
+window.addEventListener("scroll", scrollUp)
