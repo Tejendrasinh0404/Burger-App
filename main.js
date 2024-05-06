aadcff9c4a4d678dab6da2fb026380f2954cd648
Tejendrasinh <tejendrasinh6844@gmail.com>
2024-05-06 08:32:04 +0530

    added dark light mode and more features

--- main.js
+++ main.js
@@ -55,10 +55,78 @@ tabs.forEach(tab =>{
 // show scroll up 
 const  scrollUp= ()=>{
     const scrollUpBtn = document.getElementById("scroll-up");
-    if(scrollY >= 250){
-        scrollUpBtn.classList.remove("-bottom-1/2")
-        scrollUpBtn.classList.add("bottom-4")
+    if(this.scrollY >= 250){
+        scrollUpBtn.classList.remove("-bottom-1/2");
+        scrollUpBtn.classList.add("bottom-4");
     }
- 
+ else{
+    scrollUpBtn.classList.remove("bottom-4");
+    scrollUpBtn.classList.add("-bottom-1/2");
+ }
 }
 window.addEventListener("scroll", scrollUp)
+
+/*~~~~~~~~~~~~~~~ DARK LIGHT THEME ~~~~~~~~~~~~~~~*/
+const html = document.querySelector("html");
+const themeBtn = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("mode") == "dark") {
+  darkMode();
+} else {
+  lightMode();
+}
+
+themeBtn.addEventListener("click", (e) => {
+  if (localStorage.getItem("mode") == "light") {
+    darkMode();
+  } else {
+    lightMode();
+  }
+});
+
+function darkMode() {
+  html.classList.add("dark");
+  themeBtn.classList.replace("ri-moon-line", "ri-sun-line");
+  localStorage.setItem("mode", "dark");
+}
+
+function lightMode() {
+  html.classList.remove("dark");
+  themeBtn.classList.replace("ri-sun-line", "ri-moon-line");
+  localStorage.setItem("mode", "light");
+}
+
+// Scroll header  
+const  scrollHeader= ()=>{
+    const header= document.getElementById("header");
+    if(this.scrollY >= 50){
+        // scrollUpHeader.classList.remove("-bottom-1/2");
+        header.classList.add("border-b", "border-secondaryColor");
+    }
+ else{
+    header.classList.remove("border-b", "border-secondaryColor");
+    // scrollUpHeader.classList.add("-bottom-1/2");
+ }
+}
+window.addEventListener("scroll", scrollHeader)
+
+//    Scroll Section Active Link 
+const activeLink=()=>{
+  const section = document.querySelectorAll("section")
+  const navLinks = document.querySelectorAll(".nav__link")
+  let current = "home";
+  section.forEach(section => {
+    const sectionTop = section.offsetTop;
+
+    if(this.scrollY >= sectionTop - 60 ){
+current = section.getAttribute("id")
+    }
+    navLinks.forEach(item=>{
+item.classList.remove("text-secondaryColor")
+if(item.href.includes(current)){
+  item.classList.add("text-secondaryColor")
+}
+    })
+  })
+}
+
